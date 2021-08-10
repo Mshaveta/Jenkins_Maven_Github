fc7dc038a32eefe864ba3c5be8db0031e73605af
Mshaveta <shaveta.malhotra47@gmail.com>
2021-08-10 19:06:51 +0530

    Added Now

--- FB/src/test/java/FB/TestNGDemo.java
+++ FB/src/test/java/FB/TestNGDemo.java
@@ -1,5 +1,6 @@
 package FB;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -11,5 +12,9 @@ public class TestNGDemo {
 	public void m1() {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).click();
 	}
 }
