6f791959ba6794979d58ee2251ad51cdd59f6859
Mshaveta <shaveta.malhotra47@gmail.com>
2021-08-10 18:44:55 +0530

    Added

---
+++ FB/src/test/java/FB/TestNGDemo.java
@@ -0,0 +1,15 @@
+package FB;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestNGDemo {
+	@Test
+	public void m1() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+	}
+}
